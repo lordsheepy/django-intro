ab50753380241cba869dfc739a779f848f1e4dcc
Stephen Babineau <tien_ralter@hotmail.com>
2014-03-19 16:35:22 -0700

    add custom admin views

--- photoshare/admin.py
+++ photoshare/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 from photoshare.models import Photo
 from photoshare.models import Album
 from photoshare.models import Tag
 
 # Register your models here.
 
-admin.site.register(Photo)
-admin.site.register(Album)
-admin.site.register(Tag)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', )
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'owner_link', 'created', 'height', 'width')
+
+    def owner_link(self, photo):
+        url = reverse('admin:auth_user_change', args=(photo.id,))
+        name = photo.owner
+        return '<a href="%s">%s</a>' % (url, name)
+    owner_link.allow_tags = True
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner_link')
+
+    def owner_link(self, album):
+        url = reverse('admin:auth_user_change', args=(album.id,))
+        name = album.owner
+        return '<a href="%s">%s</a>' % (url, name)
+    owner_link.allow_tags = True
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Tag, TagAdmin)
--- photoshare/models.py
+++ photoshare/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 class Tag(models.Model):
     name = models.CharField(max_length=32)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Photo(models.Model):
     owner = models.ForeignKey(
@@ -14,19 +17,24 @@ class Photo(models.Model):
         related_name='photos',
         related_query_name='photo',
         )
+    height = models.IntegerField(blank=True, null=True, default=0)
+    width = models.IntegerField(blank=True, null=True, default=0)
     image = models.ImageField(
-        upload_to='/%Y/%m/%d',
+        upload_to='%Y/%m/%d',
         height_field='height',
         width_field='width',
         )
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    comment = models.CharField(max_length=144)
+    comment = models.CharField(max_length=144, blank=True)
     tags = models.ManyToManyField(
         Tag,
         related_name='tags',
         related_query_name='tag',
         )
+
+    def __unicode__(self):
+        return 'photo_id.' + unicode(self.pk)
 
 
 class Album(models.Model):
@@ -40,4 +48,9 @@ class Album(models.Model):
         Photo,
         related_name='albums',
         related_query_name='album',
+        blank=True,
+        null=True,
         )
+
+    def __unicode__(self):
+        return self.name
